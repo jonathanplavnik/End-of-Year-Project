3d4e86eea3983e8aa17ab2a0d700d45ae2f7bae7
Kay Hwang <khwang0529@gmail.com>
2024-05-21 13:50:53 -0400

    added classes

---
+++ Casino/BlackJack.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BlackJack here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BlackJack extends World
+{
+
+    /**
+     * Constructor for objects of class BlackJack.
+     * 
+     */
+    public BlackJack()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
