35604131fcf9ebbff4170e1733caaca9a2a70539
Kay Hwang <khwang0529@gmail.com>
2024-05-30 14:00:36 -0400

    random

--- Casino/Button.java
+++ Casino/Button.java
@@ -12,6 +12,7 @@ public class Button extends Actor
      * Act - do whatever the Button wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public int n1;
     public String name;
     public Button(String name){
        this.name = name; 
