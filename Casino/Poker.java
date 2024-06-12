fc22847da876053ada886c13c6fd1ac67407a257
jonathanplavnik <jonathan.plavnik@gmail.com>
2024-06-11 23:04:54 -0400

    yessir

--- Casino/Poker.java
+++ Casino/Poker.java
@@ -8,22 +8,60 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Poker extends MyWorld
 {
+    // two player game
+    private boolean questionAsked = true;
     public Poker (){
         removeButtons();
-        preflop();
-        displayQuestion();
+        System.out.println("test1");
+        //preflop();
+        
+        flop();
+        FlopBetting();
+        turn();
+        TurnBetting();
+        wash();
+        washBetting();
+        
     }
     public void act(){
+        if(questionAsked){
+            System.out.println("test2");
+            displayQuestion();
+            questionAsked = false;
+        }
         
     }
     public void displayQuestion() {
-        
-        int amt = Integer.parseInt(Greenfoot.ask("How much do you bet?"));
-        
+        System.out.println("test3");
+        int amt1 = Integer.parseInt(Greenfoot.ask("This is a two player game! How much does Player 1 bet?"));
+        System.out.println("test4");
+        int amt2 = Integer.parseInt(Greenfoot.ask("How much does Player 2 bet?"));
+        System.out.println("test5");
     }
     public void preflop(){
         cardDeck deck = new cardDeck();
         card a = deck.deck[0][0];
         System.out.println(a.getImage());
     }
+    
+    // Pre flop + deal, flop, betting, turn, betting, wash, betting
+    public void flop(){
+        
+    }
+    public void FlopBetting(){
+        
+    }
+    public void turn(){
+        
+    }
+    public void TurnBetting(){
+        
+    }
+    public void wash(){
+        
+    }
+    public void washBetting(){
+        
+    }
+    
 }
